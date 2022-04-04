9c930e9d434a8b8eb6857d0e2563f85bde2daa9f
Eric Humphries <ezhumphries@gmail.com>
2022-04-04 14:10:23 -0700

    created custom error handler and wired it into express backend

--- backend/server/index.js
+++ backend/server/index.js
@@ -3,7 +3,11 @@ import dotenv from "dotenv";
 import connectDB from "../mongo/db.js";
 import colors from "colors";
 
+// ** Routes
 import productRoutes from "../routes/productRoutes.js";
+
+// ** Middlewares
+import { notFound, errorHandler } from "../middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -12,11 +16,16 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use("/api/products", productRoutes);
-
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use("/api/products", productRoutes);
+
+// fallback for 404 errors
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(
