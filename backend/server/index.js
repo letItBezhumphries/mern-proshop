44f7132f0e293ae8ddcdd01260b3dd95acea4ad6
Eric Humphries <ezhumphries@gmail.com>
2022-04-03 18:09:43 -0700

    added in env variables to express server

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
+require("dotenv").config();
 const products = require("../data/products");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get("/", (req, res) => {
@@ -19,5 +20,7 @@ app.get("/api/products/:id", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Express server is listening on port ${PORT}`);
+  console.log(
+    `Express server is running in ${process.env.NODE_ENV} and listening on port ${PORT}`
+  );
 });
