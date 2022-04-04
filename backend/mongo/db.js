0d969fcbca2b324a6794bd3dfc2b7d8cf24a6409
Eric Humphries <ezhumphries@gmail.com>
2022-04-04 11:20:08 -0700

    added mongoose models for products, users, orders

---
+++ backend/mongo/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import colors from "colors";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const atlas = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${atlas.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/server/index.js
+++ backend/server/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "../mongo/db.js";
+import colors from "colors";
 import products from "../data/products.js";
+
 dotenv.config();
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
