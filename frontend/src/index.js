9855b722d228f537363b30aece88a805a0e54cfc
Eric Humphries <ezhumphries@gmail.com>
2022-04-04 22:15:55 -0700

    added redux store, reducer, actions, and wired up the HomeScreen component to fetch products list

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 // import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store.js";
 import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
@@ -9,6 +11,8 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
