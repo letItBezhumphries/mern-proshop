c41ce5d854ef6814cd85d7b676cf7d494c7b6142
Eric Humphries <ezhumphries@gmail.com>
2022-04-07 14:21:41 -0700

    set up backend functionality to successfully register user with password encryption

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,15 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../controllers/userController.js";
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
+router.route("/").post(registerUser);
 router.post("/login", authUser);
+// to use middleware in a route set up like below use it as a first argument
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
